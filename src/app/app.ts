cd79ca904a0e5f16ea6b2cb60022f9f1d7e3d145
MarceloCabral98sp <marcelo.cabral98sp@gmail.com>
2025-08-05 16:14:15 -0300

    feat: analizando lyfecicle challenge

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Contador } from './core-library-challenges/junior/componentes/contador/contador';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Contador, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'angular-studies-challenges';
+
+  mostrarContador = true;
 }
---
+++ src/app/core-library-challenges/junior/componentes/contador/contador.ts
@@ -0,0 +1,25 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  imports: [],
+  templateUrl: './contador.html',
+  styleUrl: './contador.scss'
+})
+export class Contador implements OnInit, OnDestroy {
+  
+  public contador = 0;
+  public intervalo: any;
+
+  ngOnInit(): void {
+    console.log('ContadorComponent inicializado');
+    this.intervalo = setInterval(() => {
+      this.contador++;
+    }, 1000)
+  }
+
+  ngOnDestroy(): void {
+    console.log('ContadorComponent destruído');
+    clearInterval(this.intervalo);
+  }
+}
